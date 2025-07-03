792d9b2498b495a905258ed8276375bd5f8b8a52
gaegnakou <gilandre.egnakou@gmail.com>
2025-07-03 11:12:33 +0000

    Add Ubuntu 24.04 quick installation scripts from GitHub

---
+++ install_ntp_monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# NTP Monitor Enterprise - Installation One-Liner Ubuntu 24.04
+# Usage: curl -fsSL https://raw.githubusercontent.com/gilandre/NTPVIZ/dev/install_ntp_monitor.sh | sudo bash
+
+# Télécharger et exécuter l'installateur complet
+curl -fsSL https://raw.githubusercontent.com/gilandre/NTPVIZ/dev/quick_install_ubuntu24.sh | sudo bash 
